1c3a0ed58adbc0b3dcae0fac6f881c003b7b1168
PhucLoc <phuclocub@gmail.com>
2024-05-16 04:19:14 +0700

    navbar and responsive

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,10 @@
+import { Navbarx } from "./navbarx";
 import { Slider } from "./slider";
 
 export default function HomePage() {
   return (
     <div>
-      <Slider />
+      <Navbarx/>
       <Slider />
     </div>
   );
