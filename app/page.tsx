7addc13302b2f2f5137fe383e0578759906da894
PhucLoc <phuclocub@gmail.com>
2024-05-20 05:12:31 +0700

    display product hardcode

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,24 @@ import { Navbarx } from "./(dashboard)/component/navbarx";
 import { Sliderx } from "./(dashboard)/component/sliderx";
 import { Sidebarx } from "./(dashboard)/component/sidebarx";
 import { Footerx } from "./(dashboard)/component/footer";
+import Products from "./products/products";
 export default function Page() {
   return (
     <div>
+      <h2>this is the home page</h2>
       <Navbarx />
-      <Sliderx />
-      <div>
-        <Sidebarx />
+      <Sliderx/>
+      <div className="flex">
+        <div className="w-3/12 mt-5">
+          <Sidebarx />
+        </div>
+        <div className="w-full bg-white">
+            <div className="ms-5 mt-5">
+            <Products/>
+            </div>
+        </div>
       </div>
-      <Footerx/>
+      <Footerx />
     </div>
   );
 }
---
+++ app/products/products.tsx
@@ -0,0 +1,60 @@
+import CellProduct from "./cellProduct";
+
+export default function Products() {
+  const data = [
+    {
+      name: "Dây da apple watch",
+      price: "840,000 VNĐ",
+      code: "APL-01",
+      description: "#",
+      url: "https://www.o2leather.com/cdn/shop/files/0T0A0311.jpg?v=1694762071",
+    },
+    {
+      name: "Dây da apple watch",
+      price: "840,000 VNĐ",
+      code: "APL-02",
+      description: "#",
+      url: "https://www.o2leather.com/cdn/shop/files/0T0A0311.jpg?v=1694762071",
+    },
+    {
+      name: "Dây da apple watch",
+      price: "840,000 VNĐ",
+      code: "APL-03",
+      description: "#",
+      url: "https://www.o2leather.com/cdn/shop/files/0T0A0311.jpg?v=1694762071",
+    },
+    {
+      name: "Dây da apple watch",
+      price: "840,000 VNĐ",
+      code: "APL-04",
+      description: "#",
+      url: "https://www.o2leather.com/cdn/shop/files/0T0A0311.jpg?v=1694762071",
+    },
+    {
+      name: "Dây da apple watch",
+      price: "840,000 VNĐ",
+      code: "APL-05",
+      description: "#",
+      url: "https://www.o2leather.com/cdn/shop/files/0T0A0311.jpg?v=1694762071",
+    },
+    {
+      name: "Dây da apple watch",
+      price: "840,000 VNĐ",
+      code: "APL-06",
+      description: "#",
+      url: "https://www.o2leather.com/cdn/shop/files/0T0A0311.jpg?v=1694762071",
+    },
+  ];
+  return (
+        <div className="w-full">
+          <h2 className="text-center text-2xl font-medium">Sản phẩm</h2>
+          <div className="flex flex-wrap">
+            {data.map((product, i) => (
+              <div className="w-4/12">
+                <CellProduct productCell={product} />
+              </div>
+            ))}
+          </div>
+        </div>
+  );
+}
