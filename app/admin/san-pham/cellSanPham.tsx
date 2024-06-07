2c7f75c0fa152d364631462dd72ac6480dde8799
PhucLoc <phuclocub@gmail.com>
2024-06-08 05:01:30 +0700

    post method sp

--- app/admin/san-pham/cellSanPham.tsx
+++ app/admin/san-pham/cellSanPham.tsx
@@ -3,11 +3,8 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 export default function CellSanPham({ cellSanPham, i }) {
   const router = useRouter();
-  const [openModal, setOpenModal] = useState(false);
-  const [email, setEmail] = useState("");
-  function onCloseModal() {
-    setOpenModal(false);
-    setEmail("");
+  function routePage(idSPCT:String){
+      console.log("route to show all detail product");
   }
   return (
     <div
@@ -46,64 +43,12 @@ export default function CellSanPham({ cellSanPham, i }) {
           Giá bán: {cellSanPham.giaBan}
         </h5>
         <a
-          onClick={() => setOpenModal(true)}
+          onClick={() => routePage("page spct")}
           className="rounded-lg bg-cyan-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
         >
           Sửa
         </a>
       </div>
-      <Modal show={openModal} size="xl" onClose={onCloseModal} popup>
-        <Modal.Header />
-        <Modal.Body>
-          <div className="space-y-6">
-            <h3 className="text-xl font-medium text-gray-900 dark:text-white">
-              Sign in to our platform
-            </h3>
-            <div>
-              <div className="mb-2 block">
-                <Label htmlFor="email" value="Your email" />
-              </div>
-              <TextInput
-                id="email"
-                placeholder="name@company.com"
-                value={email}
-                onChange={(event) => setEmail(event.target.value)}
-                required
-              />
-            </div>
-            <div>
-              <div className="mb-2 block">
-                <Label htmlFor="password" value="Your password" />
-              </div>
-              <TextInput id="password" type="password" required />
-            </div>
-            <div className="flex justify-between">
-              <div className="flex items-center gap-2">
-                <Checkbox id="remember" />
-                <Label htmlFor="remember">Remember me</Label>
-              </div>
-              <a
-                href="#"
-                className="text-sm text-cyan-700 hover:underline dark:text-cyan-500"
-              >
-                Lost Password?
-              </a>
-            </div>
-            <div className="w-full">
-              <Button>Log in to your account</Button>
-            </div>
-            <div className="flex justify-between text-sm font-medium text-gray-500 dark:text-gray-300">
-              Not registered?&nbsp;
-              <a
-                href="#"
-                className="text-cyan-700 hover:underline dark:text-cyan-500"
-              >
-                Create account
-              </a>
-            </div>
-          </div>
-        </Modal.Body>
-      </Modal>
     </div>
   );
 }
--- app/admin/san-pham/sanPhamMobile.tsx
+++ app/admin/san-pham/sanPhamMobile.tsx
@@ -9,15 +9,25 @@ import {
   ToggleSwitch,
 } from "flowbite-react";
 import { useEffect, useState } from "react";
-import { HiFolderAdd } from "react-icons/hi";
+import { HiCheckCircle, HiFolderAdd } from "react-icons/hi";
 import CellSanPham from "./cellSanPham";
 export default function SanPhamMobile() {
   const [data, setData] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
-  const [switch1, setSwitch1] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [openModal, setOpenModal] = useState(false);
-  const [email, setEmail] = useState("");
+  const [ma, setMa] = useState("");
+  const [ten, setTen] = useState("");
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, "0");
+  var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+  var yyyy = today.getFullYear();
+  var todayNow = mm + "/" + dd + "/" + yyyy;
+  var ngayTao = yyyy+"-"+mm+"-"+dd;
+  const [hinhAnh, setHinhAnh] = useState("");
+  const [giaBan, setGiaBan] = useState("");
+  const [trangThai, setTrangThai] = useState(false);
+  let validateOK = false;
   const onPageChange = (page: number) => setCurrentPage(page);
   useEffect(() => {
     fetch(
@@ -30,11 +40,39 @@ export default function SanPhamMobile() {
         console.log("data:", data);
       });
   }, []);
+  function validatorNull(textValidate: String) {
+    if (textValidate == "") {
+      validateOK = false;
+      return false;
+    } else {
+      validateOK = true;
+      return true;
+    }
+  }
   function onCloseModal() {
     setOpenModal(false);
-    setEmail("");
   }
-  function saveProduct() {}
+  function saveProduct() {
+    if (validateOK) {
+     fetch("http://ec2-54-179-249-209.ap-southeast-1.compute.amazonaws.com:8080/san-pham/save", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ma: ma,
+          ten: ten,
+          trangThai: "0",
+          ngayTao: ngayTao,
+          hinhAnh: hinhAnh,
+          giaBan: giaBan
+        }),
+      }).then(res=>console.log("test response: ", res.ok));
+    } else {
+      console.log("not do post");
+    }
+  }
   if (isLoading == false) {
     return (
       <div className="ms-1">
@@ -53,13 +91,13 @@ export default function SanPhamMobile() {
           <Modal show={openModal} size="xl" onClose={onCloseModal} popup>
             <Modal.Header />
             <Modal.Body>
-              <div className="space-y-4">
+              <div className="space-y-2">
                 <h3 className="text-xl font-medium text-gray-900 dark:text-white">
                   Nhập sản phẩm
                 </h3>
                 <div>
                   <div className="mb-2 block">
-                    <Label htmlFor="email" value="Mã sản phẩm" />
+                    <Label htmlFor="ma" value="Mã sản phẩm(fix server tự tăng)" />
                   </div>
                   <TextInput
                     id="ma"
@@ -68,27 +106,41 @@ export default function SanPhamMobile() {
                     onChange={(event) => setMa(event.target.value)}
                     required
                   />
+                  {!validatorNull(ma) ? (
+                    <p className="text-red-600">
+                      Không để trống trường dữ liệu này
+                    </p>
+                  ) : (
+                    <HiCheckCircle className="text-green-600 " />
+                  )}
                 </div>
                 <div>
                   <div className="mb-2 block">
-                    <Label htmlFor="password" value="Tên sản phẩm" />
+                    <Label htmlFor="ten" value="Tên sản phẩm" />
                   </div>
                   <TextInput
-                    id="ma"
+                    id="ten"
                     value={ten}
                     onChange={() => setTen(event.target.value)}
                     required
                   />
+                  {!validatorNull(ten) ? (
+                    <p className="text-red-600">
+                      Không để trống trường dữ liệu này
+                    </p>
+                  ) : (
+                    <HiCheckCircle className="text-green-600 " />
+                  )}
                 </div>
                 <div>
                   <div className="mb-2 block">
-                    <Label htmlFor="password" value="Ngày tạo" />
+                    <Label htmlFor="ngayTao" value="Ngày tạo" />
                   </div>
-                  <TextInput id="password" type="password" required readOnly />
+                  <TextInput id="ngayTao" value={todayNow} required readOnly />
                 </div>
                 <div>
                   <div className="mb-2 block">
-                    <Label htmlFor="hinhAnh" value="Hình ảnh" />
+                    <Label htmlFor="hinhAnh" value="Link Hình ảnh" />
                   </div>
                   <TextInput
                     id="hinhAnh"
@@ -96,10 +148,17 @@ export default function SanPhamMobile() {
                     onChange={() => setHinhAnh(event.target.value)}
                     required
                   />
+                  {!validatorNull(hinhAnh) ? (
+                    <p className="text-red-600">
+                      Không để trống trường dữ liệu này
+                    </p>
+                  ) : (
+                    <HiCheckCircle className="text-green-600 " />
+                  )}
                 </div>
                 <div>
                   <div className="mb-2 block">
-                    <Label htmlFor="password" value="Giá bán" />
+                    <Label htmlFor="giaBan" value="Giá bán" />
                   </div>
                   <TextInput
                     id="giaBan"
@@ -107,19 +166,26 @@ export default function SanPhamMobile() {
                     onChange={() => setGiaBan(event.target.value)}
                     required
                   />
+                  {!validatorNull(giaBan) ? (
+                    <p className="text-red-600">
+                      Không để trống trường dữ liệu này
+                    </p>
+                  ) : (
+                    <HiCheckCircle className="text-green-600 " />
+                  )}
                 </div>
                 <div>
                   <div className="mb-2 block">
-                    <Label htmlFor="password" value="Trạng thái" />
+                    <Label htmlFor="trangThai" value="Trạng thái" />
                   </div>
                   <ToggleSwitch
-                    checked={switch1}
+                    checked={trangThai}
                     label="Toggle me"
-                    onChange={setSwitch1}
+                    onChange={setTrangThai}
                   />
                 </div>
                 <div className="w-full">
-                  <Button>Lưu sản phẩm</Button>
+                  <Button onClick={() => saveProduct()}>Lưu sản phẩm</Button>
                 </div>
               </div>
             </Modal.Body>
