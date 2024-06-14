cca2933cb244c047445318e3e4cc9957a02cfd30
PhucLoc <phuclocub@gmail.com>
2024-06-15 01:35:32 +0700

    restructure cellspbrowser

--- app/admin/san-pham/cellSanPhamBrowser.tsx
+++ app/admin/san-pham/cellSanPhamBrowser.tsx
@@ -1,8 +1,7 @@
 import { Button, Label, Modal, TextInput, ToggleSwitch } from "flowbite-react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-export default function CellSanPhamBrowser({ cellSanPham, i, setRefParam }) {
-  const router = useRouter();
+export default function CellSanPhamBrowser({ cellSanPham, i}) {
   const [openModal, setOpenModal] = useState(false);
   const [ma, setMa] = useState(cellSanPham.ma);
   const [ten, setTen] = useState(cellSanPham.ten);
@@ -15,61 +14,18 @@ export default function CellSanPhamBrowser({ cellSanPham, i, setRefParam }) {
   const [hinhAnh, setHinhAnh] = useState(cellSanPham.hinhAnh);
   const [giaBan, setGiaBan] = useState(cellSanPham.giaBan);
   const [trangThai, setTrangThai] = useState(false);
-  let validateOK = false;
-  function routePage(idSPCT: String) {
-    router.push("/admin/san-pham/detail/" + idSPCT);
-    console.log("route to show all detail product: ", idSPCT);
-  }
-  function validatorNull(textValidate: String) {
-    if (textValidate == "") {
-      validateOK = false;
-      return false;
-    } else {
-      validateOK = true;
-      return true;
-    }
-  }
-  function onCloseModal() {
-    setOpenModal(false);
-  }
-  function updateProduct() {
-    if (validateOK) {
-      setRefParam(1);
-      fetch(
-        "http://ec2-54-179-249-209.ap-southeast-1.compute.amazonaws.com:8080/san-pham/save",
-        {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            ma: ma,
-            ten: ten,
-            trangThai: "0",
-            ngayTao: ngayTao,
-            hinhAnh: hinhAnh,
-            giaBan: giaBan,
-          }),
-        },
-      ).then((res) => console.log("test response: ", res.ok));
-    } else {
-      console.log("not do post");
-    }
-  }
-
   return (
     <div
       // onClick={() => router.push("/products/detail/" + cellSanPham.id)}
-      className="mt-5 flex flex-cols gap-[50px]"
+      className="mt-5 flex flex-cols w-10/12"
     >
-      <h5 className="text-xl tracking-tight text-gray-900 dark:text-white w-5 flex items-center">
+      <h5 className="text-xl tracking-tight text-gray-900 dark:text-white flex items-center w-1/12">
         {i + 1}{" "}
       </h5>
-      <div className="flex items-center">
+      <div className="flex items-center w-1/12">
       <img className="w-[70px] h-[50px]" src={cellSanPham.hinhAnh} alt="" />
       </div>
-      <h5 className="trackfing-tight text-xl text-gray-900 dark:text-white w-1/12 flex items-center ">
+      <h5 className="trackfing-tight text-xl text-gray-900 dark:text-white w-1/12 flex items-center  ">
         {cellSanPham.ma}{" "}
       </h5>
       <h5 className="text-xl tracking-tight text-gray-900 dark:text-white w-2/12 flex items-center">
@@ -79,101 +35,11 @@ export default function CellSanPhamBrowser({ cellSanPham, i, setRefParam }) {
         {cellSanPham.ngayTao}
       </h5>
       <h5 className="text-xl text-gray-900 dark:text-white w-1/12 flex items-center">
-        {!(cellSanPham.giaBan == 1) ? "Hoạt động" : "Dừng hoạt động"}
+        {!(cellSanPham.trangThai == 1) ? "Hoạt động" : "Dừng hoạt động"}
       </h5>
       <h5 className="text-xl text-gray-900 dark:text-white w-1/12 flex items-center">
         {cellSanPham.giaBan}
       </h5>
-      <div className="flex-cols flex gap-1 w-2/12">
-        <Button className="w-4/12" onClick={() => setOpenModal(true)}>Sửa</Button>
-        <Button className="w-4/12" onClick={() => routePage(cellSanPham.id)}>Quản lý</Button>
-      </div>
-      <Modal show={openModal} size="xl" onClose={onCloseModal} popup>
-        <Modal.Header />
-        <Modal.Body className="overflow-auto">
-          <div className="space-y-2">
-            <h3 className="text-xl font-medium text-gray-900 dark:text-white">
-              Sửa sản phẩm
-            </h3>
-            <div>
-              <div className="flex justify-center">
-                <img className="h-[200px]" src={cellSanPham.hinhAnh} alt="" />
-              </div>
-              <div className="mb-2 block">
-                <Label htmlFor="ten" value="Tên sản phẩm" />
-              </div>
-              <TextInput
-                id="ten"
-                value={ten}
-                onChange={() => setTen(event.target.value)}
-                required
-              />
-              {!validatorNull(ten) ? (
-                <p className="text-red-600">
-                  Không để trống trường dữ liệu này
-                </p>
-              ) : (
-                ""
-              )}
-            </div>
-            <div>
-              <div className="mb-2 block">
-                <Label htmlFor="ngayTao" value="Ngày tạo" />
-              </div>
-              <TextInput id="ngayTao" value={todayNow} required readOnly />
-            </div>
-            <div>
-              <div className="mb-2 block">
-                <Label htmlFor="hinhAnh" value="Link Hình ảnh" />
-              </div>
-              <TextInput
-                id="hinhAnh"
-                value={hinhAnh}
-                onChange={() => setHinhAnh(event.target.value)}
-                required
-              />
-              {!validatorNull(hinhAnh) ? (
-                <p className="text-red-600">
-                  Không để trống trường dữ liệu này
-                </p>
-              ) : (
-                ""
-              )}
-            </div>
-            <div>
-              <div className="mb-2 block">
-                <Label htmlFor="giaBan" value="Giá bán" />
-              </div>
-              <TextInput
-                id="giaBan"
-                value={giaBan}
-                onChange={() => setGiaBan(event.target.value)}
-                required
-              />
-              {!validatorNull(giaBan) ? (
-                <p className="text-red-600">
-                  Không để trống trường dữ liệu này
-                </p>
-              ) : (
-                ""
-              )}
-            </div>
-            <div>
-              <div className="mb-2 block">
-                <Label htmlFor="trangThai" value="Trạng thái" />
-              </div>
-              <ToggleSwitch
-                checked={trangThai}
-                label="Toggle me"
-                onChange={setTrangThai}
-              />
-            </div>
-            <div className="w-full">
-              <Button onClick={() => updateProduct()}>Lưu sản phẩm</Button>
-            </div>
-          </div>
-        </Modal.Body>
-      </Modal>
     </div>
   );
 }
--- app/admin/san-pham/sanPham.tsx
+++ app/admin/san-pham/sanPham.tsx
@@ -1,24 +1,37 @@
 import { Footerx } from "@/app/(dashboard)/component/footer";
 import Navbarx from "@/app/(dashboard)/component/navbarx";
-import { Button, Label, Modal, Pagination, TextInput, ToggleSwitch } from "flowbite-react";
+import {
+  Button,
+  Label,
+  Modal,
+  Pagination,
+  TextInput,
+  ToggleSwitch,
+} from "flowbite-react";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { HiFolderAdd } from "react-icons/hi";
-import CellSanPhamBrowser from "./cellSanPhamBrowser";
-import 'react-loading-skeleton/dist/skeleton.css';
 import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import CellSanPhamBrowser from "./cellSanPhamBrowser";
 export default function SanPham() {
+  const router = useRouter();
   const [data, setData] = useState();
   const [currentPage, setCurrentPage] = useState(1);
   const onPageChange = (page: number) => setCurrentPage(page);
   const [isLoading, setIsLoading] = useState(true);
-  const [openModal, setOpenModal] = useState(false);
-  const [ten, setTen] = useState("");
+  const [openModalAdd, setOpenModalAdd] = useState(false);
+  const [openModalEdit, setOpenModalEdit] = useState(false);
   var today = new Date();
   var dd = String(today.getDate()).padStart(2, "0");
   var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
   var yyyy = today.getFullYear();
   var todayNow = mm + "/" + dd + "/" + yyyy;
-  var ngayTao = yyyy + "-" + mm + "-" + dd;
+  var ngayTaoSys = yyyy + "-" + mm + "-" + dd;
+  const [id, setId] = useState("");
+  const [ma, setMa] = useState("");
+  const [ten, setTen] = useState("");
+  const [ngayTao, setNgayTao] = useState(ngayTaoSys);
   const [hinhAnh, setHinhAnh] = useState("");
   const [giaBan, setGiaBan] = useState("");
   const [trangThai, setTrangThai] = useState(false);
@@ -35,11 +48,38 @@ export default function SanPham() {
         console.log("data:", data);
       });
   }, [refkey]);
-  function onCloseModal() {
-    setOpenModal(false);
+  function onCloseModalAdd() {
+    setOpenModalAdd(false);
     console.log("test ref: ", refkey);
   }
-  
+  function onOpenModalEdit(spParam: object) {
+    setId(spParam.id);
+    setMa(spParam.ma);
+    setTen(spParam.ten);
+    setTrangThai(spParam.trangThai);
+    setNgayTao(spParam.ngayTao);
+    setHinhAnh(spParam.hinhAnh);
+    setGiaBan(spParam.giaBan);
+  }
+  function onCloseModalEdit() {
+    setOpenModalEdit(false);
+    resetState();
+    console.log("test ref: ", refkey);
+  }
+  function resetState(){
+    setId("");
+    setMa("");
+    setTen("");
+    setTrangThai(false);
+    setNgayTao("");
+    setHinhAnh("");
+    setGiaBan("");
+  }
+
+  function doSetRefKey() {
+    setRefkey(1);
+    console.log("test ref: ", refkey);
+  }
   function validatorNull(textValidate: String) {
     if (textValidate == "") {
       validateOK = false;
@@ -48,6 +88,11 @@ export default function SanPham() {
       validateOK = true;
       return true;
     }
+  }
+
+  function routePage(idSPCT: String) {
+    router.push("/admin/san-pham/detail/" + idSPCT);
+    console.log("route to show all detail product: ", idSPCT);
   }
 
   function saveProduct() {
@@ -73,53 +118,206 @@ export default function SanPham() {
       console.log("not do post");
     }
   }
-
-    return (
-      <div className="ms-2 bg-white">
-        <h2>this is the admin san pham page</h2>
-        <Navbarx />
-        <div className="me-[115px] flex flex-row-reverse">
-          <Button gradientMonochrome="info" onClick={() => setOpenModal(true)}>
-            <HiFolderAdd size={20} />
-            Thêm sản phẩm
-          </Button>
-          <Button onClick={()=>setRefkey(1)}>refresh</Button>
-        </div>
-        {!isLoading
-        ?(        <div className="z-0 w-full mt-5">
-          <div className="flex-cols md-[20px] flex gap-[50px]">
-            <h5 className="w-5 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+  function updateProduct() {
+    if (validateOK) {
+      fetch(
+        "http://ec2-54-179-249-209.ap-southeast-1.compute.amazonaws.com:8080/san-pham/save",
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            ten: ten,
+            trangThai: "0",
+            ngayTao: ngayTao,
+            hinhAnh: hinhAnh,
+            giaBan: giaBan,
+          }),
+        },
+      ).then((res) => console.log("test response: ", res.ok));
+    } else {
+      console.log("not do post");
+    }
+  }
+  return (
+    <div className="ms-2 bg-white">
+      <h2>this is the admin san pham page</h2>
+      <Navbarx />
+      <div className="me-[115px] flex flex-row-reverse">
+        <Button gradientMonochrome="info" onClick={() => setOpenModalAdd(true)}>
+          <HiFolderAdd size={20} />
+          Thêm sản phẩm
+        </Button>
+        <Button onClick={() => setRefkey(1)}>refresh</Button>
+      </div>
+      {!isLoading ? (
+        <div className="z-0 mt-5 w-full">
+          <div className="flex flex-cols md-[20px]">
+            <h5 className="w-1/12 text-xl font-semibold tracking-tight text-gray-900 dark:text-white border">
               STT
             </h5>
-            <h5 className="w-1/12 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+            <h5 className="w-1/12 text-xl font-semibold tracking-tight text-gray-900 dark:text-white border">
               Hình ảnh
             </h5>
-            <h5 className="trackfing-tight w-1/12 text-xl font-semibold text-gray-900 dark:text-white">
+            <h5 className="w-1/12 trackfing-tight text-xl font-semibold text-gray-900 dark:text-white border">
               Mã
             </h5>
-            <h5 className="w-2/12 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+            <h5 className="w-2/12 text-xl font-semibold tracking-tight text-gray-900 dark:text-white border">
               Tên sản phẩm
             </h5>
-            <h5 className="w-1/12 text-xl font-semibold text-gray-900 dark:text-white">
+            <h5 className="w-1/12 text-xl font-semibold text-gray-900 dark:text-white border">
               Ngày tạo
             </h5>
-            <h5 className="w-1/12 text-xl font-semibold text-gray-900 dark:text-white">
+            <h5 className="w-1/12 text-xl font-semibold text-gray-900 dark:text-white border">
               Trạng thái
             </h5>
-            <h5 className="w-1/12 text-xl font-semibold text-gray-900 dark:text-white">
+            <h5 className="w-1/12 text-xl font-semibold text-gray-900 dark:text-white border">
               Giá bán
             </h5>
-            <h5 className="w-2/12 text-xl font-semibold text-gray-900 dark:text-white">
+            <h5 className="w-2/12 text-xl font-semibold text-gray-900 dark:text-white border">
               Hành động
             </h5>
             <hr />
           </div>
           {data.map((sp, i) => (
             <div>
-              <CellSanPhamBrowser cellSanPham={sp} i={i} setRefParam={setRefkey()} />
+              <div className="flex-cols flex">
+                <CellSanPhamBrowser cellSanPham={sp} i={i} />
+                <div className="flex-cols flex w-2/12 gap-1">
+                  <Button
+                    className="w-4/12"
+                    onClick={() => {
+                      setOpenModalEdit(true), onOpenModalEdit(sp);
+                    }}
+                  >
+                    Sửa
+                  </Button>
+                  <Button className="w-4/12" onClick={() => routePage(sp.id)}>
+                    Quản lý
+                  </Button>
+                </div>
+              </div>
               <hr />
             </div>
           ))}
+          <Modal
+            show={openModalEdit}
+            size="xl"
+            onClose={onCloseModalEdit}
+            popup
+          >
+            <Modal.Header />
+            <Modal.Body className="overflow-auto">
+              <div className="space-y-2">
+                <h3 className="text-xl font-medium text-gray-900 dark:text-white">
+                  Sửa sản phẩm
+                </h3>
+                <div className="flex justify-center">
+                  <img className="h-[200px]" src={hinhAnh} alt="" />
+                </div>
+                <div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="ma" value="Mã sản phẩm" />
+                  </div>
+                  <TextInput
+                    id="ma"
+                    value={ma}
+                    onChange={() => setMa(event.target.value)}
+                    required
+                  />
+                  {!validatorNull(ma) ? (
+                    <p className="text-red-600">
+                      Không để trống trường dữ liệu này
+                    </p>
+                  ) : (
+                    ""
+                  )}
+                </div>
+                <div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="ten" value="Tên sản phẩm" />
+                  </div>
+                  <TextInput
+                    id="ten"
+                    value={ten}
+                    onChange={() => setTen(event.target.value)}
+                    required
+                  />
+                  {!validatorNull(ten) ? (
+                    <p className="text-red-600">
+                      Không để trống trường dữ liệu này
+                    </p>
+                  ) : (
+                    ""
+                  )}
+                </div>
+                <div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="ngayTao" value="Ngày tạo" />
+                  </div>
+                  <TextInput id="ngayTao" value={todayNow} required readOnly />
+                </div>
+                <div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="hinhAnh" value="Link Hình ảnh" />
+                  </div>
+                  <TextInput
+                    id="hinhAnh"
+                    value={hinhAnh}
+                    onChange={() => setHinhAnh(event.target.value)}
+                    required
+                  />
+                  {!validatorNull(hinhAnh) ? (
+                    <p className="text-red-600">
+                      Không để trống trường dữ liệu này
+                    </p>
+                  ) : (
+                    ""
+                  )}
+                </div>
+                <div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="giaBan" value="Giá bán" />
+                  </div>
+                  <TextInput
+                    id="giaBan"
+                    value={giaBan}
+                    onChange={() => setGiaBan(event.target.value)}
+                    required
+                  />
+                  {!validatorNull(giaBan) ? (
+                    <p className="text-red-600">
+                      Không để trống trường dữ liệu này
+                    </p>
+                  ) : (
+                    ""
+                  )}
+                </div>
+                <div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="trangThai" value="Trạng thái" />
+                  </div>
+                  <ToggleSwitch
+                    checked={trangThai}
+                    label="Toggle me"
+                    onChange={setTrangThai}
+                  />
+                </div>
+                <div className="w-full">
+                  <Button
+                    onClick={() => {
+                      updateProduct();
+                      
+                    }}
+                  >
+                    Lưu sản phẩm
+                  </Button>
+                </div>
+              </div>
+            </Modal.Body>
+          </Modal>
           <div className="flex overflow-x-auto sm:justify-center">
             <Pagination
               currentPage={currentPage}
@@ -127,97 +325,99 @@ export default function SanPham() {
               onPageChange={onPageChange}
             />
           </div>
-          <Modal show={openModal} size="xl" onClose={onCloseModal} popup>
-          <Modal.Header />
-          <Modal.Body className="overflow-auto">
-            <div className="space-y-2">
-              <h3 className="text-xl font-medium text-gray-900 dark:text-white">
-                Thêm sản phẩm
-              </h3>
-              <div>
-                <div className="flex justify-center">
-                  <img className="h-[200px]" alt="" />
+          <Modal show={openModalAdd} size="xl" onClose={onCloseModalAdd} popup>
+            <Modal.Header />
+            <Modal.Body className="overflow-auto">
+              <div className="space-y-2">
+                <h3 className="text-xl font-medium text-gray-900 dark:text-white">
+                  Thêm sản phẩm
+                </h3>
+                <div>
+                  <div className="flex justify-center">
+                    <img className="h-[200px]" alt="" />
+                  </div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="ten" value="Tên sản phẩm" />
+                  </div>
+                  <TextInput
+                    id="ten"
+                    value={ten}
+                    onChange={() => setTen(event.target.value)}
+                    required
+                  />
+                  {!validatorNull(ten) ? (
+                    <p className="text-red-600">
+                      Không để trống trường dữ liệu này
+                    </p>
+                  ) : (
+                    ""
+                  )}
                 </div>
-                <div className="mb-2 block">
-                  <Label htmlFor="ten" value="Tên sản phẩm" />
+                <div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="ngayTao" value="Ngày tạo" />
+                  </div>
+                  <TextInput id="ngayTao" value={todayNow} required readOnly />
                 </div>
-                <TextInput
-                  id="ten"
-                  value={ten}
-                  onChange={() => setTen(event.target.value)}
-                  required
-                />
-                {!validatorNull(ten) ? (
-                  <p className="text-red-600">
-                    Không để trống trường dữ liệu này
-                  </p>
-                ) : (
-                  ""
-                )}
-              </div>
-              <div>
-                <div className="mb-2 block">
-                  <Label htmlFor="ngayTao" value="Ngày tạo" />
+                <div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="hinhAnh" value="Link Hình ảnh" />
+                  </div>
+                  <TextInput
+                    id="hinhAnh"
+                    value={hinhAnh}
+                    onChange={() => setHinhAnh(event.target.value)}
+                    required
+                  />
+                  {!validatorNull(hinhAnh) ? (
+                    <p className="text-red-600">
+                      Không để trống trường dữ liệu này
+                    </p>
+                  ) : (
+                    ""
+                  )}
                 </div>
-                <TextInput id="ngayTao" value={todayNow} required readOnly />
-              </div>
-              <div>
-                <div className="mb-2 block">
-                  <Label htmlFor="hinhAnh" value="Link Hình ảnh" />
+                <div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="giaBan" value="Giá bán" />
+                  </div>
+                  <TextInput
+                    id="giaBan"
+                    value={giaBan}
+                    onChange={() => setGiaBan(event.target.value)}
+                    required
+                  />
+                  {!validatorNull(giaBan) ? (
+                    <p className="text-red-600">
+                      Không để trống trường dữ liệu này
+                    </p>
+                  ) : (
+                    ""
+                  )}
                 </div>
-                <TextInput
-                  id="hinhAnh"
-                  value={hinhAnh}
-                  onChange={() => setHinhAnh(event.target.value)}
-                  required
-                />
-                {!validatorNull(hinhAnh) ? (
-                  <p className="text-red-600">
-                    Không để trống trường dữ liệu này
-                  </p>
-                ) : (
-                  ""
-                )}
-              </div>
-              <div>
-                <div className="mb-2 block">
-                  <Label htmlFor="giaBan" value="Giá bán" />
+                <div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="trangThai" value="Trạng thái" />
+                  </div>
+                  <ToggleSwitch
+                    checked={trangThai}
+                    label="Toggle me"
+                    onChange={setTrangThai}
+                  />
                 </div>
-                <TextInput
-                  id="giaBan"
-                  value={giaBan}
-                  onChange={() => setGiaBan(event.target.value)}
-                  required
-                />
-                {!validatorNull(giaBan) ? (
-                  <p className="text-red-600">
-                    Không để trống trường dữ liệu này
-                  </p>
-                ) : (
-                  ""
-                )}
-              </div>
-              <div>
-                <div className="mb-2 block">
-                  <Label htmlFor="trangThai" value="Trạng thái" />
+                <div className="w-full">
+                  <Button onClick={() => saveProduct()}>Lưu sản phẩm</Button>
                 </div>
-                <ToggleSwitch
-                  checked={trangThai}
-                  label="Toggle me"
-                  onChange={setTrangThai}
-                />
               </div>
-              <div className="w-full">
-                <Button onClick={() => saveProduct()}>Lưu sản phẩm</Button>
-              </div>
-            </div>
-          </Modal.Body>
-        </Modal>
-        </div>)
-        :(<Skeleton count={20} />)}
-        <div className="mt-5">
-          <Footerx />
+            </Modal.Body>
+          </Modal>
         </div>
+      ) : (
+        <Skeleton count={20} />
+      )}
+      <div className="mt-5">
+        <Footerx />
       </div>
-    );
-  }
+    </div>
+  );
+}
