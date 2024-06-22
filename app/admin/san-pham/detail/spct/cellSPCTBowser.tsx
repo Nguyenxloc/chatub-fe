33f3975d4adb030010d75b8d3af924e59b1e5acd
PhucLoc <phuclocub@gmail.com>
2024-06-22 15:30:49 +0700

    browser spct and pending api not return chatlieu

--- app/admin/san-pham/detail/spct/cellSPCTBowser.tsx
+++ app/admin/san-pham/detail/spct/cellSPCTBowser.tsx
@@ -1,20 +1,24 @@
-
-export default function CellSPCTBrowser({ spct,indx,lstMauSac,lstKichThuoc }) {
-    return (
+export default function CellSPCTBrowser({
+  spct,
+  indx,
+  lstMauSac,
+  lstKichThuoc,
+}) {
+  return (
+    <div>
       <div>
-          <div>
-            <div className="flex flex-cols w-screen">
-              <h2 className="w-1/12 flex items-center">{indx+1}</h2>
-              <h2 className="w-1/12 flex items-center">{spct.chatLieu}</h2>
-              <h2 className="w-1/12 flex items-center">{spct.mauSac.ten}</h2>
-              <h2 className="w-1/12 flex items-center">{spct.kichThuoc.ten}</h2>
-              <h2 className="w-1/12 flex items-center">{spct.soLuongTon}</h2>
-              <h2 className="w-1/12 flex items-center">{spct.trangThai}</h2>
-              <h2 className="w-1/12 flex items-center">Hình ảnh 1</h2>
-              <h2 className="w-1/12 flex items-center">Hình ảnh 2</h2>
-              <h2 className="w-1/12 flex items-center">Hình ảnh 3</h2>
-            </div>
-          </div>
+        <div className="flex-cols flex w-screen">
+          <h2 className="flex w-1/12 items-center">{indx + 1}</h2>
+          <h2 className="flex w-1/12 items-center">{spct.chatLieu?spct.chatLieu.ten:"n/a"}</h2>
+          <h2 className="flex w-1/12 items-center">{spct.mauSac.ten}</h2>
+          <h2 className="flex w-1/12 items-center">{spct.kichThuoc.ten}</h2>
+          <h2 className="flex w-1/12 items-center">{spct.soLuongTon}</h2>
+          <h2 className="flex w-1/12 items-center">{spct.trangThai}</h2>
+          <h2 className="flex w-1/12 items-center">Hình ảnh 1</h2>
+          <h2 className="flex w-1/12 items-center">Hình ảnh 2</h2>
+          <h2 className="flex w-1/12 items-center">Hình ảnh 3</h2>
+        </div>
       </div>
-    );
+    </div>
+  );
 }
--- app/admin/san-pham/detail/spct/cellSPCTMobile.tsx
+++ app/admin/san-pham/detail/spct/cellSPCTMobile.tsx
@@ -1,18 +1,22 @@
-
-export default function CellSPCTMobile({ spct, indx, lstMauSac, lstKichThuoc }) {
+export default function CellSPCTMobile({
+  spct,
+  indx,
+  lstMauSac,
+  lstKichThuoc,
+}) {
   return (
     <div>
       <div>
         <div className="flex-cols flex w-screen">
           <h2 className="flex w-1/12 items-center text-xs">{indx + 1}</h2>
-          <h2 className="flex w-1/12 items-center text-xs">{spct.chatLieu}</h2>
-          <h2 className="flex w-1/12 ms-[20px] items-center text-xs">
+          <h2 className="flex w-1/12 items-center text-xs">{spct.chatLieu?spct.chatLieu.ten:"n/a"}</h2>
+          <h2 className="ms-[20px] flex w-1/12 items-center text-xs">
             {spct.mauSac.ten}
           </h2>
-          <h2 className="flex w-1/12 ms-[20px] items-center text-xs">
+          <h2 className="ms-[20px] flex w-1/12 items-center text-xs">
             {spct.kichThuoc.ten}
           </h2>
-          <h2 className="flex w-1/12 ms-[20px] items-center text-xs">
+          <h2 className="ms-[20px] flex w-1/12 items-center text-xs">
             {spct.soLuongTon}
           </h2>
           <h2 className="flex w-1/12 items-center text-xs">{spct.trangThai}</h2>
